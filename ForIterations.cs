078e0bfe220fe11dc588e82e2d784585309de746
Jakub Plinta <jakub.plinta@interia.pl>
2014-09-07 14:05:58 +0200

    Learning "for" iterations in c#.

---
+++ ForIterations.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ForIterations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                //Console.Write(i.ToString() + " ");
+
+                if (i == 7)
+                {
+                    Console.WriteLine("Found seven!");
+                    break;
+                }
+            }
+
+            for (int value = 0; value < 12; value++)
+            {
+                Console.Write(value + " ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
