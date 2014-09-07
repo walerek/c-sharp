e5fa4e39a3a3825c9ad4617579b98c5b15d04108
Jakub Plinta <jakub.plinta@interia.pl>
2014-09-07 18:58:23 +0200

    Helper methods in c#

---
+++ HelperMethods.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelperMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myValue = superSecretFormula("world");
+            Console.WriteLine(myValue);
+            Console.ReadLine();
+        }
+
+        private static string superSecretFormula()
+        {
+            return "Hello World!";
+        }
+
+        private static string superSecretFormula(string name)
+        {
+            return String.Format("Hello, {0}!", name);
+        }
+    }
+}
